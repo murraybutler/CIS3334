bc043665c84c8810f5a005e138122a788927ba71
Murray Butler <murray.butler@M083910BUG3QD.community.veritas.com>
2018-02-01 21:22:10 -0600

    Commits for Unit 3 assignaments

--- Converter2/app/src/main/java/edu/css/converter/MainActivity.java
+++ Converter2/app/src/main/java/edu/css/converter/MainActivity.java
@@ -1,13 +1,90 @@
 package edu.css.converter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import static java.lang.*;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inBills;
+    private TextView outBills;
+
+    double rateUS = getRate("USD");
+    double rateEU = getRate("EUR");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inBills = findViewById(R.id.inBills);
+        outBills = findViewById(R.id.outBills);
+
+        ArrayList curs = new ArrayList<>();
+
+
+    }
+
+    public void buttonDollaClick(View view){
+        double startMoney = Double.parseDouble(inBills.getText().toString());
+        double outMoney = calcRate("USD", "EUR", startMoney);
+        outBills.setText(Double.toString(outMoney));
+    }
+
+    public void buttonEuroClick(View view) {
+        double startMoney = Double.parseDouble(inBills.getText().toString());
+        double outMoney = calcRate("EUR", "USD", startMoney);
+        outBills.setText(Double.toString(outMoney));
+    }
+
+    private double calcRate(String base, String comp, double amt) {
+        double rate = getRate(base, comp);
+        double outMoney = amt / rate;
+    }
+
+    private double getRate(String base, String comp) {
+        //RequestQueue queue = Volley.newRequestQueue(this);
+
+        String strurl = "http://api.fixer.io/latest?base=" + base + "&symbols=" + comp;
+        URLConnection urlConn;
+        BufferedReader buffRead = null;
+
+        try {
+            URL url = new URL(strurl);
+            urlConn = url.openConnection();
+            buffRead = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+            Stringbuffer strBuff = new StringBuffer();
+            String lin;
+            while ((lin = buffRead.readLine()) != null) {
+                strBuff.append(lin);
+            }
+            return new JSONObject(strBuff.toString());
+        } catch(JSONException e) {
+            Log.e("Converter", "DataGet", e);
+            return null;
+        } finally {
+            if(buffRead != null) {
+                try {
+                    buffRead.close();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
---
+++ TipCalc/app/src/main/java/edu/css/tipcalc/MainActivity.java
@@ -0,0 +1,89 @@
+package edu.css.tipcalc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int num = 0;
+    private EditText billAmt;
+    private EditText tipAmt;
+    private EditText totAmt;
+    private boolean bigTip = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        billAmt = findViewById(R.id.billAmt);
+        tipAmt = findViewById(R.id.tipAmt);
+        totAmt = findViewById(R.id.totAmt);
+        Button calc = findViewById(R.id.calcTip);
+
+        //calc.setOnClickListener(new View.OnClickListener());
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.radio1:
+                if (checked)
+                    num = 1;
+                break;
+            case R.id.radio2:
+                if (checked)
+                    num = 2;
+                break;
+            case R.id.radio3:
+                if (checked)
+                    num = 3;
+                break;
+            case R.id.radio4:
+                if (checked)
+                    num = 4;
+                break;
+        }
+    }
+
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox)view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.serviceUpg:
+                if (checked)
+                    bigTip = true;
+                break;
+        }
+    }
+
+    private void tipper(int party, boolean big) {
+        double bill = Double.parseDouble(billAmt.getText().toString());
+        double tip;
+        double tot;
+
+        if (big) {
+            tip = (bill * .2) / party;
+        } else {
+            tip = (bill * .1) / party;
+        }
+
+        tot = (bill + tip)/party;
+
+        tipAmt.setText(Double.toString(tip));
+        totAmt.setText(Double.toString(tot));
+    }
+
+    public void onClick(View view) {
+        if (num > 0) {
+            tipper(num, bigTip);
+        }
+    }
+}
